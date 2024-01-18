cdde57c9898128febb151d6bda1fb3701e6313e4
tamisekaid <156979783+tamisekaid@users.noreply.github.com>
2024-01-18 09:38:42 +0700

    Add files via upload

---
+++ obf_1eu7rJ1X0JMO5mwFjMM89rvzsMD32GJnQe5c0EXON485I1lIDLKtrzOH4qn1Ebze.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "khôi chịch kim" -- Put You Name Script Here
+_G.Logo = "16011348444" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
